1ae49266fd3171aca6f8097dedeae5f558c9190e
Xaver Fleer <xaver.fleer@gmail.com>
2021-10-19 14:45:15 +0200

    Add header and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,26 @@ import React from "react";
 import "../assets/styles/style.css";
 import * as styles from "../assets/styles/index.module.css";
 
+import img from "../assets/images/huge-bread.jpg";
+
 export default function Page() {
   return (
     <div className={styles.wrapper}>
-      <main>Here will be a page</main>
+      <header className={styles.header}>
+        <img
+          alt="Pain de la boulangerie Aux 1000 Feuilles"
+          className={styles.img}
+          src={img}
+        />
+        <div className={styles.headerText}>
+          <h1>Aux 1000 Feuilles</h1>
+          <h2>Boulangerie – Patisserie – Confiserie</h2>
+        </div>
+      </header>
+      <footer>
+        <div>Grand'Rue 36, 1530 Payerne</div>
+        <a href="tel:+41266602224">026 660 22 24</a>
+      </footer>
     </div>
   );
 }
