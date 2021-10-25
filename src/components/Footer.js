6417c3a73c19a67e830b5191809d585795036d6f
Xaver Fleer <xaver.fleer@gmail.com>
2021-10-25 17:16:57 +0200

    Add social media buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,34 @@
 import React from "react";
 import * as styles from "./Footer.module.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <div className={styles.socials}>
+        <a
+          href="https://www.facebook.com/Aux1000Feuilles/"
+          rel="noopener nofollow"
+        >
+          <StaticImage
+            alt="Facebook - Aux 1000 Feuilles"
+            as="a"
+            className={styles.social}
+            src="../assets/images/facebook.svg"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/aux1000feuilles/"
+          rel="noopener nofollow"
+        >
+          <StaticImage
+            alt="Instagram - Aux 1000 Feuilles"
+            as="a"
+            src="../assets/images/instagram.svg"
+            className={styles.social}
+          />
+        </a>
+      </div>
       <div>Grand'Rue 36, 1530 Payerne</div>
       <a href="tel:+41266602224">026 660 22 24</a>
     </footer>
