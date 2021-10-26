f181a94a30f97a4c6b66973a7450e0df5e269500
Xaver Fleer <xaver.fleer@gmail.com>
2021-10-26 11:28:57 +0200

    🧹 Refactor: remove obsolete dependeny

--- src/pages/croissant.js
+++ src/pages/croissant.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import "../assets/styles/style.css";
 
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
